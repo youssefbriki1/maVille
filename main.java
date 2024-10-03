ad5fd0472aec91244da6de99d0b9557889d37bf2
jalal <fatouaki.jalal@gmail.com>
2024-10-03 12:37:20 -0400

    commited

--- main.java
+++ main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;  
 public class main {
     public static void main(String[] args){
-        System.out.println("Bienvenue");
+        System.out.println("Enter username");
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        String userName = myObj.nextLine();  // Read user input
+        
+        switch (userName) {
+            case "2":
+                System.out.println("lol");
+                break;
+        
+            default:
+                System.out.println("hello " + userName);
+                break;
+        }
     }
 }
