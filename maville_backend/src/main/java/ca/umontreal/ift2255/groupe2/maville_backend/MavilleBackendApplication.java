8074f9dba3368c56dd9a187a4e710c6b51292d67
youssefbriki1 <youssef.briki05@gmail.com>
2024-11-04 20:00:17 -0500

    intialized front-end & backend architecture

---
+++ maville_backend/src/main/java/ca/umontreal/ift2255/groupe2/maville_backend/MavilleBackendApplication.java
@@ -0,0 +1,13 @@
+package ca.umontreal.ift2255.groupe2.maville_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavilleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavilleBackendApplication.class, args);
+	}
+
+}
